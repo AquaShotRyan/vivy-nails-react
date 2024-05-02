19d75cc6a3ba3be5a1b67f9ab45e1c7a316cc5e2
AquaShotRyan <ryantran574@gmail.com>
2024-05-02 16:43:11 -0400

    contact page prototype

--- vivy-nails/src/pages/Contact.js
+++ vivy-nails/src/pages/Contact.js
@@ -1,7 +1,26 @@
 export default function Contact() {
+	let contact = require('../json/contact.json');
+
+	// get values json file
+	let phone = contact["phone"];
+	let address = contact["address"];
+  let hours = contact["hours"]
+
 	return (
-		<div>
-			
+		<div className="contact-container">
+			<div className="contact-col">
+        {phone}
+			</div>
+			<div className="contact-col">
+        {
+          Object.keys(hours).map(key => {
+            return <p>{key + " " + hours[key]}</p>
+          })
+        }
+      </div>
+			<div className="contact-col">
+        {address}
+      </div>
 		</div>
 	)
 }
