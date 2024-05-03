ee7701cc8cfb78a07bada580c1e34883610a32e8
AquaShotRyan <ryantran574@gmail.com>
2024-05-03 15:13:01 -0400

    changed design of the header

--- vivy-nails/src/layouts/RootLayout.js
+++ vivy-nails/src/layouts/RootLayout.js
@@ -5,26 +5,42 @@ export default function RootLayout() {
     <div className="root-layout">
       <ScrollRestoration />
       <header>{/* white header section */}
+      
+        <div className="header-side">
 
-        <div className="header-logo">
-          <NavLink to="/">
-            <img src="./vivy-nails-logo.png" alt="VIVY NAILS"></img>
-          </NavLink>
         </div>
 
-        <div className="header-navbar">
-          <nav>
-            <NavLink to="Services">Services</NavLink>
-            <NavLink to="Gallery">Gallery</NavLink>
-            <NavLink to="Contact">Contact</NavLink>
-          </nav>
+        <div className="header-middle">
+          <div className="header-logo">
+            <NavLink to="/">
+              <img src="./vivy-nails-logo.png" alt="VIVY NAILS"></img>
+            </NavLink>
+          </div>
+
+          <div className="header-navbar">
+            <nav>
+              <div>
+                <NavLink to="Services">Services</NavLink>
+              </div>
+              <div>
+                <NavLink to="Gallery">Gallery</NavLink>                
+              </div>
+              <div>
+                <NavLink to="Contact">Contact</NavLink>                
+              </div>
+            </nav>
+          </div>
+        </div>
+        
+        <div className="header-side">
+          <div className="header-socials">
+            <a href="https://www.instagram.com/vivy_nails_official/">
+              <img src ="./instagram.svg" alt="Instagram"/>
+            </a>
+          </div>
         </div>
 
-        <div className="header-socials">
-          <a href="https://www.instagram.com/vivy_nails_official/">
-            <img src ="./instagram.svg" alt="Instagram"/>
-          </a>
-        </div>
+        
 
       </header>
       <main>
