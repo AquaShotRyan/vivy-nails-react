0efef59e9ae2ffc357e7e6c78d296276def92ff2
AquaShotRyan <ryantran574@gmail.com>
2024-05-12 15:11:12 -0400

    Added marquee to home page

--- vivy-nails/src/pages/Contact.js
+++ vivy-nails/src/pages/Contact.js
@@ -4,23 +4,42 @@ export default function Contact() {
 	// get values json file
 	let phone = contact["phone"];
 	let address = contact["address"];
-  let hours = contact["hours"]
+  let hours = contact["hours"];
+
+	/* {
+			Object.keys(hours).map(key => {
+				return <p>{key + " " + hours[key]}</p>
+			})
+			} */
 
 	return (
 		<div className="contact-container">
-			<div className="contact-col">
-        {phone}
+
+			<div className="contact-salon-address">
+				<h2>Salon Address</h2>
+				<p>{address}</p>
+				<p>{phone}</p>
+				<p>By appointment or walk-in</p>
 			</div>
-			<div className="contact-col">
-        {
-          Object.keys(hours).map(key => {
-            return <p>{key + " " + hours[key]}</p>
-          })
-        }
-      </div>
-			<div className="contact-col">
-        {address}
-      </div>
+
+			<div className="contact-break"></div>
+
+			<div className="contact-hours">
+				<h2>Hours</h2>
+				<table>
+					{
+						Object.keys(hours).map(key => {
+							return( 
+								<tr>
+									<td>{key}</td>
+									<td className="contact-time">{hours[key]}</td>
+								</tr>
+							)
+						})
+					}
+				</table>
+			</div>
+
 		</div>
 	)
 }
--- vivy-nails/src/pages/Home.js
+++ vivy-nails/src/pages/Home.js
@@ -1,4 +1,4 @@
-
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
   let contact = require('../json/contact.json');
@@ -7,19 +7,32 @@ export default function Home() {
   let phone = contact["phone"];
   let address = contact["address"];
 
+  // images to display
+  const images = [
+    {src: "./home-images/photo_1.jpg", alt: "nails1"},
+    {src: "./home-images/photo_1.jpg", alt: "nails1"},
+    {src: "./home-images/photo_1.jpg", alt: "nails1"},
+    {src: "./home-images/photo_1.jpg", alt: "nails1"},
+    {src: "./home-images/photo_1.jpg", alt: "nails1"},
+    {src: "./home-images/photo_1.jpg", alt: "nails1"},
+  ]
+
   return (
     <div className="home-container">
 
-      <div className="home-left">
-        <p>Call to book an appointment today!</p>
-        <br/>
-        <p>{phone}</p>
-        <br/>
-        <p>{address}</p>
+      <div className="home-top">
+        <p>Vivy Nails is a locally owned nail salon located in Ottawa</p>
+        <p>Walk-in or book an appointment by calling<br></br>{phone}</p>
+
+        <Marquee className="home-marquee" gradient={true} gradientWidth={70} gradientColor="#C7A7AA">
+          {images.concat(images, images).map((image, index) => (
+            <img src={image.src} alt={image.alt}></img>
+          ))}
+        </Marquee>
       </div>
 
-      <div className="home-right">
-        <img src="./home-images/photo_1.jpg" alt="nails"></img>
+      <div className="home-bottom">
+        <p>Find Vivy Nails at <br></br>{address}</p>
       </div>
 
     </div>
